a7e00fecc595dd328682a465292805e8f3298c05
p70m3th3us <p70m3th3us@gmail.com>
2022-02-24 19:34:49 -0600

    add basic functionality

--- mathhelper/__init__.py
+++ mathhelper/__init__.py
@@ -1,1 +1,3 @@
 """command line application to help individuals learn quick math"""
+
+from mathhelper.core import main as testme
--- mathhelper/core.py
+++ mathhelper/core.py
@@ -1,6 +1,21 @@
 """core function of mathhelper"""
 
+import random
+
 
 def main():
-    """print hello world"""
-    print("hello world")
+    """main function of the mathhelper package"""
+    possibilities = [[i, j] for i in range(1, 21) for j in range(i, 21)]
+    while True:
+        problem = random.choice(possibilities)
+        answer = problem[0] * problem[1]
+        print(f"{problem[0]} * {problem[1]}")
+        user_input = input("::")
+        try:
+            if user_input == "stop":
+                break
+            elif int(user_input) != (answer):
+                print(f"incorrect, {answer}")
+        except ValueError:
+            print("input is not accepted buddy, get less stoopid")
+    print("program ending")
